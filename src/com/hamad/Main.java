8bfc0adfb9574c7296198281350ae1fa4aab4272
Hamad Ali <hammadali248@gmail.com>
2019-11-29 16:15:10 +0000

    first commit

---
+++ src/com/hamad/Main.java
@@ -0,0 +1,96 @@
+package com.hamad;
+
+// Using Variables
+/*
+public class Main {
+
+    public static void main(String[] args) {
+       int myNumber=99;
+       short myShortNumber = 32_000;
+       long myLongNumber = 300_000_000;
+
+       double myDoubleDecimalNumber =7.2345;
+       float myFloatShortDecimalNumber =423.3F;
+
+       char myChar = 'Y';
+       boolean doIHaveAJob = true;
+
+       byte myByte = 127;
+
+        System.out.println(myNumber);
+        System.out.println(myShortNumber);
+        System.out.println(myLongNumber);
+        System.out.println(myDoubleDecimalNumber);
+        System.out.println(myFloatShortDecimalNumber);
+        System.out.println(myChar);
+        System.out.println(doIHaveAJob);
+        System.out.println(myByte);
+    }
+}
+*/
+
+
+
+//Strings
+/*
+public class Main {
+
+    public static void main(String[] args) {
+
+        int age = 19;
+
+        String  text = "Hello! How Are You?";
+        String blank = " ";
+        String name = "Bob";
+
+        String greeting = text + blank + name;
+
+        System.out.println(greeting);
+
+        System.out.println(greeting + " My age is: " + age);
+
+        System.out.println(text + " My name is " + name + ".My age is "+ age + ".");
+
+
+    }
+}
+*/
+
+
+
+// While loops
+/*
+public class Main {
+
+    public static void main(String[] args) {
+        int value = 0;
+
+        while (value < 10){
+            System.out.println("Hello Person Number: " + value);
+            value = value + 1;
+        }
+
+    }
+}
+*/
+
+
+
+// For Loops
+/*
+public class Main {
+
+    public static void main(String[] args) {
+
+        for(int i=0; i < 1000; i ++) {
+            System.out.println("Hello Friend " + i );
+        }
+
+    }
+}
+*/
+
+
+
+
+
